ea1577cf690eb0c9f6c663b5d95af1993c8067ea
Chakra <info@chakra.digital>
2021-05-24 10:48:56 -0500

    frontend going now

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = () => (
+  <Fragment>
+    <h1>Heyyy</h1>
+    <p>really stoked about this 🤠</p>
+  </Fragment>
+);
+
+export default App;
